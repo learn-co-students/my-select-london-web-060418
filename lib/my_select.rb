ecd2e60c7db3dfe75c0717df5f40ca043bea7e89
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-22 13:13:26 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  new_array = []
+
+    for element in array
+      if yield(element) == TRUE
+        new_array.push element
+      end
+    end
+
+  new_array
 end
